49c7d57ae9dab5ff394997c1fa686264aaa1eb40
ale <rimoldi@ditoy.com>
2017-06-26 15:32:38 +0200

    use the tiny template engine to include the html template

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once('vendor/autoload.php');
+
 function debug($label, $value) {
 	echo("<pre>$label: ".str_replace('<', '&lt;', $print_r($value, 1))."</pre>\n");
 }
@@ -61,19 +64,11 @@ $content->add(new Text('text', 'title', 'h1'));
 $content->add(new Text('textarea', "<ul>\n<li>first item</li>\n<li>a second one</li>\n</ul>"));
 $content->add(new Image('img/tv-test.png'));
 
+$template = new \Ditoy\Tiny\Template();
+$template->set('content', $content);
+if (file_exists('project/template/index.php')) {
+	echo $template->fetch('project/template/index.php');;
+} else {
+	echo $template->fetch('template/index.php');;
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>title</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
-  </head>
-  <body>
-		<p><a href="?edit">edit</a> <a href="?">no edit</a></p>
-		<div class="content">
-			<?= $content->render() ?>
-		</div>
-  </body>
-</html>
