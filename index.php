a0f990324c2caea22d45f387f7d811b19c14a629
ale <rimoldi@ditoy.com>
2017-06-26 11:49:20 +0200

    use a fake aggregator with text and an image

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+function debug($label, $value) {
+	echo("<pre>$label: ".str_replace('<', '&lt;', $print_r($value, 1))."</pre>\n");
+}
+class Id {
+	public static function next() {
+		static $id = 0;
+		$id++;
+		return $id;
+	}
+}
+class Text
+{
+	private $type = null;
+	private $content = null;
+	private $style = null;
+	private $id = null;
+	public function __construct($type, $content, $style = null) {
+		$this->type = $type;
+		$this->content = $content;
+		$this->style = $style;
+		$this->id = Id::next();
+	}
+	public function render() {
+		if ($this->type == 'text') {
+			return "<".$this->style.">".$this->content."</".$this->style.">";
+		} elseif ($this->type == 'textarea') {
+			return "<div>".$this->content."</div>";
+		}
+	}
+}
+
+class Image
+{
+	private $url = null;
+	private $id = null;
+	public function __construct($url) {
+		$this->url = $url;
+		$this->id = Id::next();
+	}
+	public function render() {
+		return "<img src=\"".$this->url."\">";
+	}
+}
+
+class Aggregator
+{
+	private $item = [];
+	public function add($item) {
+		$this->item[] = $item;
+	}
+	public function render() {
+		return implode("\n", array_map(function($i) { return $i->render();}, $this->item));
+	}
+}
+
+$content = new Aggregator();
+$content->add(new Text('text', 'title', 'h1'));
+$content->add(new Text('textarea', "<ul>\n<li>first item</li>\n<li>a second one</li>\n</ul>"));
+$content->add(new Image('img/tv-test.png'));
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,13 +71,7 @@
   <body>
 		<p><a href="?edit">edit</a> <a href="?">no edit</a></p>
 		<div class="content">
-			<h1>My site</h1>
-			<p>This is my site</p>
-			<ul>
-				<li>first item</li>
-				<li>a second one</li>
-			</ul>
-			<img src="img/tv-test.png">
+			<?= $content->render() ?>
 		</div>
   </body>
 </html>
