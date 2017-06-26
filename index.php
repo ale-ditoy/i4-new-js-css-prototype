db446e4108d0fbefd0f7e3e890d0c5562bc8c24f
ale <rimoldi@ditoy.com>
2017-06-26 11:28:39 +0200

    pure html version

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+  </head>
+  <body>
+		<p><a href="?edit">edit</a> <a href="?">no edit</a></p>
+		<div class="content">
+			<h1>My site</h1>
+			<p>This is my site</p>
+			<ul>
+				<li>first item</li>
+				<li>a second one</li>
+			</ul>
+			<img src="img/tv-test.png">
+		</div>
+  </body>
+</html>
