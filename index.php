bece458cda5fd4ae9f53581648a9f006e8350ca6
ale <rimoldi@ditoy.com>
2017-07-07 11:15:37 +0200

    add the index changes made for the js branch

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include_once('vendor/autoload.php');
 
 function debug($label, $value) {
-    echo("<pre>$label: ".str_replace('<', '&lt;', $print_r($value, 1))."</pre>\n");
+    echo("<pre>$label: ".str_replace('<', '&lt;', print_r($value, 1))."</pre>\n");
 }
 
 class Id {
@@ -20,17 +20,22 @@ class Text
     private $content = null;
     private $style = null;
     private $id = null;
+    private $encloser = null;
+    public function getEncloser() { return  $this->encloser; }
     public function __construct($type, $content, $style = null) {
         $this->type = $type;
         $this->content = $content;
         $this->style = $style;
         $this->id = Id::next();
+        $this->encloser = new ContentEncloser();
     }
     public function render() {
+        $this->encloser->addClass('i4-edit-text');
         if ($this->type == 'text') {
-            return "<".$this->style.">".$this->content."</".$this->style.">";
+            $this->encloser->setTag($this->style);
+            return $this->encloser->renderOpen().$this->content.$this->encloser->renderClose();
         } elseif ($this->type == 'textarea') {
-            return "<div>".$this->content."</div>";
+            return $this->encloser->renderOpen()."\n".$this->content."\n".$this->encloser->renderClose();
         }
     }
 }
@@ -39,12 +44,15 @@ class Image
 {
     private $url = null;
     private $id = null;
+    private $encloser = null;
+    public function getEncloser() { return  $this->encloser; }
     public function __construct($url) {
         $this->url = $url;
         $this->id = Id::next();
+        $this->encloser = new ContentEncloser();
     }
     public function render() {
-        return "<img src=\"".$this->url."\">";
+        return $this->encloser->renderOpen().'<img src="'.$this->url.'">'.$this->encloser->renderClose();
     }
 }
 
@@ -57,8 +65,47 @@ class Aggregator
     public function render() {
         return implode("\n", array_map(function($i) { return $i->render();}, $this->item));
     }
+    public function render_edit() {
+       $items = array_map(function($i) {
+            $i->getEncloser()->addClass('i4-edit');
+            return $i->render();
+        }, $this->item);
+        return implode("\n", $items);
+    }
 }
 
+class ContentEncloser
+{
+    private $tag = 'div';
+    public function setTag($tag) { $this->tag = $tag; }
+    private $id = null;
+    public function setId($id) { $this->id = $id; }
+    private $class = [];
+    public function addClass($class) { $this->class[] = $class; }
+    private $data = [];
+    public function addData($key, $data) { $this->data[$key] = $value; }
+
+    public function renderOpen() {
+        $result = [];
+        $result[] = $this->tag;
+        if (isset($this->id)) {
+            $result[] = 'id = "'.$this->id.'"';
+        }
+        if (!empty($this->class)) {
+            $result[] = 'class = "'.implode(' ', $this->class).'"';
+        }
+        if (!empty($this->class)) {
+            $result[] = 'class = "'.implode(' ', $this->class).'"';
+        }
+        foreach ($this->data as $key => $value) {
+            $result[] = 'data-'.$key.' = "'.$this->value.'"';
+        }
+        return '<'.implode(' ', $result).'>';
+    }
+    public function renderClose() {
+        return '</'.$this->tag.'>';
+    }
+ }
 class Content
 {
     private $aggregator = null;
@@ -66,7 +113,11 @@ class Content
         $this->aggregator = $aggregator;
     }
     public function render() {
-        return $this->aggregator->render();
+        if (array_key_exists('edit', $_REQUEST)) {
+            return $this->aggregator->render_edit();
+        } else {
+            return $this->aggregator->render();
+        }
     }
 }
 
@@ -77,8 +128,23 @@ $aggregator->add(new Image('img/tv-test.png'));
 
 $content = new Content($aggregator);
 
+$script = [];
+if (array_key_exists('edit', $_REQUEST)) {
+    $script[] = 'js/edit.js';
+}
+
+$css = [
+    'css/style.css'
+];
+if (array_key_exists('edit', $_REQUEST)) {
+    $css[] = 'css/font-awesome.min.css';
+    $css[] = 'css/edit.css';
+}
+
 $template = new \Ditoy\Tiny\Template();
 $template->set('content', $content);
+$template->set('style', implode("\n", array_map(function($s) { return '<link rel="stylesheet" href="'.$s.'">'; }, $css)));
+$template->set('script', implode("\n", array_map(function($s) { return '<script src="'.$s.'"></script>'; }, $script)));
 if (file_exists('project/template/index.php')) {
     echo $template->fetch('project/template/index.php');;
 } else {
